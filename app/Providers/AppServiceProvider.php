c374e6a600fac9638f0223ec34e21da4f80c9eb2
Avinash Kakade <kakadeavinash9@gmail.com>
2025-04-27 01:20:25 +0530

    https issue fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         // Use View::composer('*', function()) to share data with all views
         View::composer('*', function ($view) {
             $user = null;
